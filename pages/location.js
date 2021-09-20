906f81400b6981b37727822a0a5bdb157db472c9
Matt Coles <mattjcoles@outlook.com>
2021-09-20 18:36:15 +1000

    added pauls endpoint

--- pages/location.js
+++ pages/location.js
@@ -4,20 +4,20 @@ import axios from 'axios';
 import styles from '../styles/Home.module.css'
 import NavigationBar from '../components/navigation_bar';
 
-const Location = ({ comment, id }) => {
+const Location = ({ coffeePlace, id }) => {
     return (
         <div className={styles.container}>
             <NavigationBar/>
-            <h1>{comment[0].name}</h1>
-            <p>{comment[0].body}</p>
+            <h1>{coffeePlace.locationName}</h1>
+            <p>{coffeePlace.locationDescription}</p>
         </div>
     );
 };
 
 Location.getInitialProps = async({ query }) => {
-    const res = await axios.get(`https://jsonplaceholder.typicode.com/comments?id=${query.id}`);
-    const commentData = res.data;
-    return { id: query.id, comment: commentData};
+    const res = await axios.get(`https://pdbkut2xpg.ap-northeast-1.awsapprunner.com/coffee/${query.id}`);
+    const coffeeData = res.data;
+    return { id: query.id, coffeePlace: coffeeData};
 };
 
 export default Location;
